db11298a5f77bbfb58ab73c533f41146d96f959f
nirmal199930 <nirmalchundru@gmail.com>
2024-09-03 10:42:49 +0530

    till date

--- expressServer/public/js/form.js
+++ expressServer/public/js/form.js
@@ -1,30 +1,30 @@
 console.log("hello world");
 
 
-let getbtn=document.getElementById("get")
+let getbtn = document.getElementById("get")
 
-getbtn.addEventListener("click",()=>{
+getbtn.addEventListener("click", () => {
     fetch("/getStudent").then(res => res.json())
-    .then(data => console.log(data))
-    .catch(error => console.error())
+        .then(data => console.log(data))
+        .catch(error => console.error())
 })
 
 
-let postbtn=document.getElementById("post")
-let student={
-    name:"akhil",
-    age:24,
-    address:"khammam"
+let postbtn = document.getElementById("post")
+let student = {
+    name: "nirmal",
+    age: 24,
+    address: "khammam"
 }
-postbtn.addEventListener("click",()=>{
-fetch("/postData",{
-    method:"POST",
-    headers:{
-        "content-type":"application/json",
-        "message":"this is a student data"
-    },
-    body:JSON.stringify(student)
-}).then(res => res.json())
-.then(data => console.log(data))
-.catch(error => console.log(error))
+postbtn.addEventListener("click", () => {
+    fetch("/postData", {
+        method: "POST",
+        headers: {
+            "content-type": "application/json",
+            "message": "this is a student data"
+        },
+        body: JSON.stringify(student)
+    }).then(res => res.json())
+        .then(data => console.log(data))
+        .catch(error => console.log(error))
 })
---
+++ postmanCRUD/routes/userRoute.js
@@ -0,0 +1,19 @@
+const express = require("express")
+
+const { userSignup, getUser, updateUser, deleteUser } = require('../controller/userControllers')
+
+const userRouter = express.Router()
+
+userRouter.get("/", (req, res) => {
+    res.send("This is user route")
+})
+
+userRouter.post("/signup", userSignup)
+
+userRouter.post("/getuser", getUser)
+
+userRouter.put("/update/:mobile", updateUser)
+
+userRouter.delete("/delete", deleteUser)
+
+module.exports = { userRouter }
